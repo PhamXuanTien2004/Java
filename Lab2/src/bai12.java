00bcdc00caa3711702774ee7973838588d90f6fc
PhamXuanTien2004 <2221050418@student.humg.edu.vn>
2024-01-31 12:07:50 +0700

    baitap31/01

---
+++ Lab2/src/bai12.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class bai12 {
+    public static void main(String[] args) {
+        int n,m;
+        System.out.print("So hang la: ");
+        Scanner sc= new Scanner(System.in);
+        n=sc.nextInt();
+        System.out.print("So cot la: ");
+        m=sc.nextInt();
+        int A[][]=new int[n][m];
+        int max=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                System.out.print("Phan tu thu ["+i+","+j+"] la: ");
+                A[i][j]=sc.nextInt();
+                if(A[i][j]>max)
+                max=A[i][j];
+            }
+        }
+        System.out.print("Gia tri lon nhat trong ma tran la: "+max);
+        sc.close();
+    }
+}
