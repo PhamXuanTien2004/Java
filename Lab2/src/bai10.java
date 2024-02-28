caa691821e2256198b3939540f6f8048c19482cd
PhamXuanTien2004 <2221050418@student.humg.edu.vn>
2024-02-28 10:22:43 +0700

    bth 28/02

--- Lab2/src/bai10.java
+++ Lab2/src/bai10.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 public class bai10 {
     public static void main(String[] args) {
         String chuoi;
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Chuoi la: ");
-        chuoi=sc.nextLine();
-        char c=chuoi.charAt(0);
-        int s=1;
-        for(int i=1;i<chuoi.length();i++)
-        if(c==chuoi.charAt(i))
-        s++;
-        System.out.println("So phan xuat hien la: "+s);
+        char kytu;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap chuoi: ");
+        chuoi = sc.nextLine();
+
+        System.out.print("Nhap ky tu: ");
+        kytu = sc.next().charAt(0);
+
+        int s = 0;
+        for (int i = 0; i < chuoi.length(); i++) {
+            if (kytu == chuoi.charAt(i)) {
+                s++;
+            }
+        }
+
+        System.out.println("So lan xuat hien cua ky tu '" + kytu + "' trong chuoi la: " + s);
         sc.close();
     }
 }
