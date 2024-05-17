2294cfde7d65e78868cc8c26cbe7bdd884f01bb0
PhamXuanTien2004 <153746591+PhamXuanTien2004@users.noreply.github.com>
2024-05-18 05:17:40 +0700

    18/5 s86

--- DemoC4/src/TreeSet/bai1.java
+++ DemoC4/src/TreeSet/bai1.java
@@ -1,31 +1,17 @@
 package TreeSet;
-import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
-import java.util.Scanner;
 public class bai1 {
     public static void main(String[] args) {
-        int number;
-        TreeSet<Integer> treeSetInteger =new TreeSet<Integer>();
-        Scanner sc= new Scanner(System.in);
-        treeSetInteger.add(0);
-        treeSetInteger.add(3);
-        treeSetInteger.add(1);
-        treeSetInteger.add(4);
-        treeSetInteger.add(2);
-        treeSetInteger.add(8);
-        System.out.println("cac phan tu trong treeSetInteger");
-        System.out.println(treeSetInteger);
-        System.out.println("Nhap phan tu can them ");
-        number = sc.nextInt();
-        if(!treeSetInteger.contains(number)){
-            treeSetInteger.add(number);
-            System.out.println("them thanh cong ");
-            System.out.println("Cac phan tu trong hashSetInteger sau khi them ");
-            System.out.println(treeSetInteger);
-
-        }else{
-            System.out.println("Phan tu "+number+ "da ton tai ");
-        }sc.close();
-
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("CSLT","Co so lap trinh");
+        hashMap.put("C++","C++");
+        hashMap.put("C#","C sharp");
+        hashMap.put("PHP","PHP");
+        hashMap.put("Java","Java");
+        Set<Map.Entry<String , String>> setHashMap = hashMap.entrySet();
+        System.out.println("Cac entry co trong hashmap");
+        System.out.println(setHashMap);
     }
 }
