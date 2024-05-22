34f5a95f25421a5d296804ad5a4fbc967ddcbb5f
PhamXuanTien2004 <153746591+PhamXuanTien2004@users.noreply.github.com>
2024-05-22 09:39:57 +0700

    PXT418_Cau2

--- BaiKiemTra/src/PXT418_Person.java
+++ BaiKiemTra/src/PXT418_Person.java
@@ -1,8 +1,26 @@
 import java.util.Scanner;
 
+
 public class PXT418_Person {
     public int tuoi;
     public String ten;
     public String que;
+    public void nhap()
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Nhap ten: ");
+        ten=sc.nextLine();
+        System.out.println("Tuoi: ");
+        tuoi=sc.nextInt();
+        System.out.println("Que quan: ");
+        que=sc.nextLine();
+    }
+    public void xuat()
+    {
+        System.out.println("Thong tin cua Person");
+        System.out.println("Ten la "+ten);
+        System.out.println("Tuoi "+tuoi);
+        System.out.println("Que quan "+que);
+    }
     
 }
